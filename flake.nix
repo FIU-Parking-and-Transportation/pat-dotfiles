b3f17c70ac9ce7182793d32126d32f8c4a2ce6f2
Gentman Tan <gentmantan@gmail.com>
2026-01-26 12:49:51 -0500

    Add dokploy

--- flake.nix
+++ flake.nix
@@ -3,17 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nix-dokploy.url = "github:el-kurto/nix-dokploy";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nix-dokploy,
     ...
   }: {
     nixosConfigurations = {
       server = nixpkgs.lib.nixosSystem {
         modules = [
           ./hosts/server/configuration-server.nix
+          nix-dokploy.nixosModules.default
+          {
+            virtualisation.docker.enable = true;
+            virtualisation.docker.daemon.settings.live-restore = false;
+            services.dokploy.enable = true;
+          }
         ];
       };
     };
